d6418d8d5a7236df34b322f241ff409682deedab
KSYong <ericyong95@naver.com>
2022-11-13 10:00:20 +0900

    Initial Commit

---
+++ MyCreditManager/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  MyCreditManager
+//
+//  Created by 권승용 on 2022/11/12.
+//
+
+import Foundation
+
+while true {
+    // MARK: - 메뉴
+    print("원하는 기능을 입력해주세요")
+    print("1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
+    
+    
+    
+}
